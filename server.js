3dd9c465560249fbaf3b7be243928e134df69689
Azra-Rizqia <azrarizqia@gmail.com>
2024-01-27 12:50:30 +0700

    update port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
